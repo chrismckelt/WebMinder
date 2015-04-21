e54bae19a68f2e51ec2f9962109c0b642fc52ed9
chris mckelt <chris@mckelt.com>
2015-04-21 23:25:59 +0800

    clean

--- WebSiteTest/Global.asax.cs
+++ WebSiteTest/Global.asax.cs
@@ -23,16 +23,14 @@ namespace Charon.WebSiteTest
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            // rules sets
+            // ip ruleset - disallow more than 20 requests per day from a logged 'failed'  request
             var ipAnalyserRule = new RequestAnalyserRuleSet<IRuleRequest>(HttpApplicationStorage)
             {
-                AggregateRule = ip => ip.Where(a => a.CreatedUtcDateTime >= DateTime.UtcNow.AddDays(-1))
-                    //.Select(b => b.IpAddress)
-                    .Count() > 2,
+                AggregateRule = ip => ip.Count(a => a.CreatedUtcDateTime >= DateTime.UtcNow.AddDays(-1)) > 20,
             };
 
 
-            RuleSetRunner.Instance.AddRule((RequestAnalyserRuleSet<IRuleRequest>)ipAnalyserRule);
+            RuleSetRunner.Instance.AddRule(ipAnalyserRule);
         }
 
         private IList<IRuleRequest> HttpApplicationStorage()
