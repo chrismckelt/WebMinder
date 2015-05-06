00255729fb5765c3e4fd8c775a7d602a71dc9191
Chris McKelt <chris@mckelt.com>
2015-05-06 14:36:26 +0800

    valid url checker

---
+++ Core/Rules/ValidUrlRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebMinder.Core.Rules
+{
+    public class ValidUrlRequest :IRuleRequest
+    {
+        public Uri Endpoint { get; set; }
+        public DateTime CreatedUtcDateTime { get; set; }
+    }
+}
